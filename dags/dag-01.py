27dbee0e591409066c59b7953fa648f0c3ae2cde
Ivan Kostrubin <7spawn@gmail.com>
2024-12-17 23:47:56 +0200

    adding dags

--- dags/dag-01.py
+++ dags/dag-01.py
@@ -3,32 +3,28 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
-# Параметры DAG
 default_args = {
     'owner': 'airflow',
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-# Инициализация DAG
+# init
 with DAG(
-    dag_id='example_dag',
+    dag_id='hello_moto',
     description='Пример DAG для демонстрации работы Airflow',
-    schedule_interval='@daily',  # DAG запускается раз в сутки
+    schedule_interval='@daily',
     start_date=datetime(2024, 6, 1),
-    catchup=False,  # Отключить запуск DAG для прошедших дат
+    catchup=False,
     default_args=default_args,
 ) as dag:
 
-    # Функция для PythonOperator
     def print_hello_world():
-        print("Hello, World! DAG успешно запустился.")
+        print("Hello, MOTO! DAG успешно запустился.")
 
-    # PythonOperator - задача с вызовом функции
     hello_task = PythonOperator(
         task_id='print_hello_world_task',
         python_callable=print_hello_world
     )
 
-    # Зависимости задач
     hello_task
