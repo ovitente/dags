0f9975e5d580744ec8df9f7e348a1a182333e5ab
Ivan Kostrubin <7spawn@gmail.com>
2024-12-24 02:14:16 +0200

    Update dag-01.py

--- dags/dag-01.py
+++ dags/dag-01.py
@@ -1,7 +1,7 @@
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+import time
 
 default_args = {
     'owner': 'airflow',
@@ -9,22 +9,38 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+# Функция для нагрузки
+def cpu_intensive_task(task_id):
+    print(f"Запуск задачи {task_id}")
+    # Создание вычислительной нагрузки
+    result = 0
+    for i in range(1, 10**7):
+        result += i
+    print(f"Задача {task_id} завершена с результатом {result}")
+
 # init
 with DAG(
-    dag_id='hello_moto',
-    description='Пример DAG для демонстрации работы Airflow',
+    dag_id='heavy_load_dag',
+    description='DAG для нагрузочного тестирования',
     schedule_interval='@daily',
     start_date=datetime(2024, 6, 1),
     catchup=False,
     default_args=default_args,
+    concurrency=10,  # Ограничение на количество задач, которые могут одновременно выполняться в рамках DAG
+    max_active_runs=3,  # Ограничение на количество одновременно выполняющихся DAG
 ) as dag:
 
-    def print_hello_world():
-        print("Hello, MOTO! DAG успешно запустился.")
+    # Создание 50 задач для параллельных вычислений
+    tasks = []
+    for i in range(50):
+        task = PythonOperator(
+            task_id=f'cpu_intensive_task_{i}',
+            python_callable=cpu_intensive_task,
+            op_args=[f'task_{i}'],
+            task_concurrency=5,  # Параллельные вычисления для каждой задачи
+        )
+        tasks.append(task)
 
-    hello_task = PythonOperator(
-        task_id='print_hello_world_task',
-        python_callable=print_hello_world
-    )
-
-    hello_task
+    # Задачи выполняются параллельно
+    for task in tasks:
+        task
